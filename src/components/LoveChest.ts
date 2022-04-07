eca1c235b630bbf268d74ab418792c3abb643695
Arnav <66426125+Advik274@users.noreply.github.com>
2022-04-07 07:56:00 +0530

    Update LoveChest.ts

--- src/components/LoveChest.ts
+++ src/components/LoveChest.ts
@@ -75,7 +75,7 @@ export class LoveChest {
 
 	private createFlyingText() {
 		this.flyingText = new FlyingText({
-			text: 'Happy Birthday, Utsha!',
+			text: 'Happy Birthday, My Utsha! 😃',
 			style: {
 				fontFamily: 'Arcade',
 				fontSize: '32px',
